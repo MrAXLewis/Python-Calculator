76b2250c76f5a611c2c70eca2af8a707badd665f
Axel Lewis <axelr_lewis@hotmail.com>
2020-11-28 15:21:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+def welcome():
+    print('''
+  Welcome to Axel's Calculator!
+  ''')
+
+# Define our function
+def calculate():
+    operation = input(''' 
+Please type in the math operation you would like to complete:
++ for Addition
+- for Subtraction
+* for Multiplication
+/ for division
+** for power
+% for modulo
+''')
+
+
+
+    number_1 = int(input('Enter your first number: '))
+    number_2 = int(input('Enter your second number: '))
+
+# Addition
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+
+# Subtraction
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+
+# Multiplication
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+
+# Division
+    elif operation == '/':
+          print('{} / {} = '.format(number_1, number_2))
+          print(number_1 / number_2)
+
+# Power
+    elif operation == '**':
+        print('{} ** {} ='.format(number_1, number_2))
+        print(number_1 ** number_2)
+
+# Modulo
+    elif operation == '%':
+        print('{} % {} ='. format(number_1, number_2))
+        print(number_1 % number_2)
+    
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+
+
+  # Add again() function to calculate() function
+    again()
+
+def again():
+  calc_again = input('''Do you want to calculate again?
+  Please type Y for YES or N for NO.''')
+
+  if calc_again.upper() == 'Y':
+    calculate()
+  elif calc_again.upper() == 'N':
+    print('See you later.')
+  else:
+    again()
+
+welcome()
+calculate()
